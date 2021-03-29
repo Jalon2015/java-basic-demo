4baa7127b89fff5b452165c7900cc1870432e9f9
Jalon2015 <1121263265@qq.com>
2021-03-29 23:29:30 +0800

    1

---
+++ src/com/jalon/basic/array/ArrayDemo.java
@@ -0,0 +1,11 @@
+package com.jalon.basic.array;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        String[] arr = {"1","2"};
+        arr[3] = "c";
+        System.out.println(Arrays.toString(arr));
+    }
+}
